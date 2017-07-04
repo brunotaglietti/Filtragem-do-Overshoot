6e4c334605fb5bdf90e86ce4fbc0659c9c8a56e7
brunotag <brunotaglietti@gmail.com>
2017-07-04 17:26:01 -0300

    Checking errors

--- Overshoot_Filter_5.m
+++ Overshoot_Filter_5.m
@@ -1,4 +1,3 @@
-close all; clc;
 addpath('functions', 'plots');
 
 %% UI Configuration and Memory Allocation
@@ -14,27 +13,23 @@ mse_char = struct(eF{:}); ber = struct(eF{:}); errors = cell(1,2); clear M i eF;
 t_start = tic;
 for B = 1:length(bias)
 for V = 1:length(deg)
-cur_var = [bias(B), deg(V), deg(V), bits];
-signal = syncd_import(charinfo,cur_var,tech); % Import
-[switched, s_info] = sw_cycle(signal); % Cycle cropping
-[yout, mse_char(V,B), ber(V,B), errors{1}] = sw_filter(switched.y_s, switched.xs_slice, s_info);
-[~, ~, ~, errors{2}] = sw_filter(switched.Norm.y_s, switched.xs_slice, s_info);
-toc(t_start);
+    cur_var = [bias(B), deg(V), deg(V), bits];
+    signal = syncd_import(charinfo,cur_var,tech); % Import
+    [switched, s_info] = sw_cycle(signal); % Cycle cropping
+    [yout, mse_char(V,B), ber(V,B), errors{1}] = sw_filter(switched.y_s, switched.xs_slice, s_info);
+    [~, ~, ~, errors{2}] = sw_filter(switched.Norm.y_s, switched.xs_slice, s_info);
+    toc(t_start);
 end
 end
 
 %% PLOTS
 % close all; fignum = 1;
 if length(deg) == 1 && length(bias) == 1, NE = zeros(s_info.N_cycles,1);
-errorDistPlot(s_info, errors);
-
-for n = 1:s_info.N_cycles
-    NE(n) = sum([errors{1}.w2{n}(5:end-5)]) - sum([errors{1}.s{n}(5:end-5)]);
-%     if NE < sum([errors{1}.s{n}]), NE = sum([errors{1}.s{n}]);
-%         cyPlot(signal, switched, s_info, yout, n, errors);
-%         waitforbuttonpress;
-%     end
-end
+    errorDistPlot(s_info, errors);
+    for n = 1:s_info.N_cycles
+        NE(n) = sum(errors{1}.w{n}(5:end-5)) - sum(errors{1}.s{n}(5:end-5));
+    end
+    [~,n] = max(NE); cyPlot(signal, switched, s_info, yout, n, errors);
 elseif length(deg) == 1 && length(bias) > 1, bias_plot(bias, mse_char, 'MSE');
 elseif length(deg) > 1, VIplot(bias, deg, mse_char, 'MSE', [0 1]);
     VIplot(bias, deg, ber, 'BER',[-3 -1]);
