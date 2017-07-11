009dab7c2570d1274757b7c0ccda5aca75bd754c
brunotag <brunotaglietti@gmail.com>
2017-07-11 16:16:05 -0300

    saving results
    BERxGI figures saved to the characterization directory.
    
    Results saved include PISIC, MISIC, bias of 80, 100 and 120 mA. Step amplitude is 1.2 V.

--- plots/errorDistPlot.m
+++ plots/errorDistPlot.m
@@ -3,19 +3,20 @@ fprintf('Guard-Interval Analysis and plot.\n')
 %%
 if length(errors) == 1,  eFields = fieldnames(errors)';
 else, eFields = fieldnames(errors{1})'; end
-if exist('pTitle','var'), figure('name', pTitle);
-else, figure; end
-errorDist = zeros(1,max(s_info.Samp_Cy));
-for N = 1:length(errors)
-    if length(errors)>1, cE = errors{N}; else, cE = errors; end
-    for f = 1:length(eFields)
-        for k = 1:s_info.N_cycles
-            errorDist(1:s_info.Samp_Cy(k)) = errorDist(1:s_info.Samp_Cy(k)) + cE.(eFields{f}){k};
-        end
-        subplot(length(errors),length(eFields), (f + (N-1)*length(eFields)))
-        bar(errorDist), title(eFields{f});
-    end
-end
+% if exist('pTitle','var'), figure('name', pTitle);
+% else, figure; end
+% errorDist = zeros(1,max(s_info.Samp_Cy));
+% for N = 1:length(errors)
+%     if length(errors)>1, cE = errors{N}; else, cE = errors; end
+%     for f = 1:length(eFields)
+%         for k = 1:s_info.N_cycles
+%             curL = length(cE.(eFields{f}){k});
+%             errorDist(1:curL) = errorDist(1:curL) + cE.(eFields{f}){k};
+%         end
+%         subplot(length(errors),length(eFields), (f + (N-1)*length(eFields)))
+%         bar(errorDist), title(eFields{f});
+%     end
+% end
 %%
 maxGI = 25; endcrop = 5;
 M = zeros(1,maxGI+1);
@@ -34,11 +35,11 @@ for N = 1:length(errors)
             GIerror.(eFields{f})(GI+1) = GIerror.(eFields{f})(GI+1)/...
             (sum(s_info.Samp_Cy)-GI*s_info.N_cycles);
         end
-        LGIerror = log10(GIerror.(eFields{f}));
-        if LGImin > min(LGIerror(LGIerror~=-Inf)), LGImin = min(LGIerror(LGIerror~=-Inf)); end
+        LGIerror = GIerror.(eFields{f});
+%         if LGImin > min(LGIerror(LGIerror~=-Inf)), LGImin = min(LGIerror(LGIerror~=-Inf)); end
         LGIerror(LGIerror==-Inf) = -100;
-        plot(0:maxGI,LGIerror,plotStyle{N,:}), hold on; ylim([LGImin -1]);
-        xlabel('Guard-Interval'), ylabel('Log_{10} (BER)');
+        plot(0:maxGI,LGIerror,plotStyle{N,:}), hold on; %ylim([LGImin -1]);
+        xlabel('Guard-Interval'), ylabel('BER'); set(gca, 'YScale', 'log')
         if exist('pTitle','var'), title(upper(pTitle)); end
     end
     ax = gca; ax.ColorOrderIndex = 1;
@@ -48,4 +49,5 @@ eNormLeg = eFields; for N = 1:length(eFields), eNormLeg{N} = ['Norm ' eFields{N}
 content = {eFields, eNormLeg}; legend([content{:}]), grid on
 else, legend(eFields), grid on
 end
+
 end
--- plots/tech_comp_v2.m
+++ plots/tech_comp_v2.m
@@ -1,25 +1,35 @@
+rootdir = 'E:\Projetos Colaborativos\chav-amo-SOA-prbs\CIP-L\';
 addpath('functions', 'plots');
 
-% tech = {'pisic', 'misic'};
-tech = {'pisic'};
 %% UI Configuration and Memory Allocation
-if ~exist('charinfo','var')
-    [FileName,Path, ~] = uigetfile('E:\Projetos Colaborativos\chav-amo-SOA-prbs\CIP-L\');
-    load([Path FileName]); clear FileName;
-end
-bits = 8; bias = 0.1; deg = 1.2;
+% if ~exist('charinfo','var')
+%     [FileName,Path, ~] = uigetfile(rootdir); load([Path FileName]); clear FileName;
+% end
+
+for bias = [0.08 0.1 0.12]
+deg = 1.2;
 eF = {'s', 'w', 'w2', 'rls', 'rls2'}; M = zeros(length(deg),length(bias));
 for i=1:length(eF), eF{2,i} = M; end;
 mse_char = struct(eF{:}); ber = struct(eF{:}); errors = cell(1,2); clear M i eF;
 
 %%
+for bits = [2 4 8]
 tStart = tic;
 cur_var = [bias, deg, deg, bits];
+tech = {'pisic', 'misic'};
+span = {'syncd', 'syncd_b2b_brief'};
+for m = 1:length(span)
+clear charinfo, load([rootdir span{m} '\charinfo.mat']);
 for k = 1:length(tech)
     signal  = syncd_import(charinfo, cur_var, tech{k});
     [switched, s_info] = sw_cycle(signal);
-    [yout, mse, ber, errors{k}] = sw_filter(switched.y_s, switched.xs_slice, s_info);
+    [yout, mse, ber, errors{1}] = sw_filter(switched.y_s, switched.xs_slice, s_info);
     [~, ~, ~, errors{2}] = sw_filter(switched.Norm.y_s, switched.xs_slice, s_info);
-    errorDistPlot(s_info, errors, tech{k});
+    errorDistPlot(s_info, errors, sprintf([tech{k} '-%i'], int16(bits)));
+    curdir = [rootdir span{m} sprintf(['\\' lower(tech{k}) '-%i\\'], int16(bits))];
+    saveas(gcf, [curdir sprintf('BERxGI_%.1fV_%imA.png',deg,int16(1e3*bias))], 'png');
     toc(tStart);
+end
+end
+end
 end
