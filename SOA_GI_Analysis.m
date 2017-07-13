f203146009d5ea307987809d9eb2df6028051d46
brunotag <brunotaglietti@gmail.com>
2017-07-13 18:07:44 -0300

    UI fix, import update and renaming
    UI menu now changes if deg is only a set value.
    
    Function syncd_import.m is now soah5import.m, and adapts the directories and names to each measurement.
    
    Overshoot_Filter_5.m is now SOA_GI_Analysis.m.

--- SOA_GI_Analysis.m
+++ SOA_GI_Analysis.m
@@ -14,10 +14,10 @@ t_start = tic;
 for B = 1:length(bias)
 for V = 1:length(deg)
     cur_var = [bias(B), deg(V), deg(V), bits];
-    signal = syncd_import(charinfo,cur_var,tech); % Import
+    signal = soah5import(charinfo,cur_var,tech); % Import
     [switched, s_info] = sw_cycle(signal); % Cycle cropping
     [yout, mse_char(V,B), ber(V,B), errors{1}] = sw_filter(switched.y_s, switched.xs_slice, s_info);
-%     [~, ~, ~, errors{2}] = sw_filter(switched.Norm.y_s, switched.xs_slice, s_info);
+    [~, ~, ~, errors{2}] = sw_filter(switched.Norm.y_s, switched.xs_slice, s_info);
     toc(t_start);
 end
 end
@@ -25,7 +25,7 @@ end
 %% PLOTS
 % close all;
 if length(deg) == 1 && length(bias) == 1, NE = zeros(s_info.N_cycles,1);
-%     errorDistPlot(s_info, errors);
+    errorDistPlot(s_info, errors);
     for n = 1:s_info.N_cycles
         NE(n) = sum(errors{1}.s{n}(1+5:end-5)) - sum(errors{1}.w{n}(1+5:end-5));
     end
