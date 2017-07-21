f6d0ff5101e3a39949c5b2bf6080c1dbbf9a3a18
brunotag <brunotaglietti@gmail.com>
2017-07-21 15:58:52 -0300

    BERxGI
    Stores the results of the analysis in a variable, and saves it to the Results folder.

--- plots/errorDistPlot.m
+++ plots/errorDistPlot.m
@@ -1,4 +1,4 @@
-function errorDistPlot(s_info, errors, pTitle)
+function BERxGI = errorDistPlot(s_info, errors, pTitle)
 fprintf('Guard-Interval Analysis and plot.\n')
 if length(errors) == 1,  eFields = fieldnames(errors)';
 else, eFields = fieldnames(errors{1})'; end
@@ -22,7 +22,7 @@ else, eFields = fieldnames(errors{1})'; end
 maxGI = 25; endcrop = 5;
 M = zeros(1,maxGI+1);
 eFalloc = eFields; for i=1:length(eFields), eFalloc{2,i} = M; end
-GIerror = struct(eFalloc{:}); clear M eFalloc i;
+BERxGI = struct(eFalloc{:}); clear M eFalloc i;
 figure;
 LGImin = 0; plotStyle = {'-o', 'linewidth',2; '-+','linewidth',1};
 for N = 1:length(errors)
@@ -30,13 +30,13 @@ for N = 1:length(errors)
     for f = 1:length(eFields)
         for GI = 0:maxGI
             for k = 1:s_info.N_cycles
-                GIerror.(eFields{f})(GI+1) = GIerror.(eFields{f})(GI+1) + ...
+                BERxGI.(eFields{f})(GI+1) = BERxGI.(eFields{f})(GI+1) + ...
                     sum(cE.(eFields{f}){k}(1+GI:end-endcrop));
             end
-            GIerror.(eFields{f})(GI+1) = GIerror.(eFields{f})(GI+1)/...
+            BERxGI.(eFields{f})(GI+1) = BERxGI.(eFields{f})(GI+1)/...
             (sum(s_info.Samp_Cy)-GI*s_info.N_cycles);
         end
-        LGIerror = GIerror.(eFields{f});
+        LGIerror = BERxGI.(eFields{f});
         if LGImin > min(LGIerror(LGIerror~=0)), LGImin = min(LGIerror(LGIerror~=-Inf)); end
 %         LGIerror(LGIerror==-Inf) = -100;
         plot(0:maxGI,LGIerror,plotStyle{N,:}), hold on; %ylim([LGImin -1]);
--- plots/tech_comp_v2.m
+++ plots/tech_comp_v2.m
@@ -3,46 +3,48 @@ rootdir = 'E:\Projetos Colaborativos\chav-amo-SOA-prbs\CIP-L - 2017.07.20\';
 addpath('complementary', 'functions', 'plots');
 spans = {'SSMF', 'NZD_25', 'NZD_50', 'NZD_75',...
     'NZD_25_DC', 'NZD_50_DC', 'NZD_75_DC'};
+swTechs = {'pisic', 'misic'};
+bitses = [2, 4, 8];
+biasses = [.08, .10, .12];
+BERxGI = cell(  length(spans),...
+                length(biasses),...     0.08    0.10    0.12
+                length(swTechs)+1,...   step    pisic   misic
+                length(bitses)); %      2(0)     4       8
 
+tStart = tic;
 for S = 1:length(spans)
 curdir = [rootdir spans{S} '\'];
 load([curdir 'charinfo.mat']);
-for bias = charinfo.cur
+for ib = 1:length(charinfo.cur)
+bias = biasses(ib);
 deg = charinfo.deg; imp = charinfo.imp;
-eF = {'s', 'w', 'w2', 'rls', 'rls2'}; M = zeros(length(deg),length(bias));
-for i=1:length(eF), eF{2,i} = M; end;
-mse_char = struct(eF{:});
-clear M i eF;
-
-%%
-tStart = tic;
-
 tech = 'step';
 cur_var = [bias, deg, 0, 0];
 signal = soah5import(charinfo,cur_var,tech); % Import
 [switched, s_info] = sw_cycle(signal);
-[yout, mse, ber, errors] = sw_filter(switched.y_s, switched.xs_slice, s_info);
-errorDistPlot(s_info, errors, tech);
-saveas(gcf, [rootdir 'BERxGI_' spans{S} '_' sprintf('%imA_',int16(1e3*bias)),...
-          'Step.png'], 'png');
-toc(tStart);
-swTechs = {'pisic', 'misic'};
+[~, ~, ber, errors] = sw_filter(switched.y_s, switched.xs_slice, s_info);
+BERxGI{S,ib,1,1} = errorDistPlot(s_info, errors, [spans{S} ' - ' tech]);
+saveas(gcf, ['.\Results\BERxGI_' spans{S} '_' sprintf('%imA_',int16(1e3*bias)),...
+         tech '.png'], 'png');
+toc(tStart); close all;
 for T = 1:length(swTechs)
-for bits = [2 4 8]
+for ibits = 1:length(bitses)
+    bits = bitses(ibits);
     tech = swTechs{T};
     spTech = sprintf([tech '-%i'], int16(bits));
     cur_var = [bias, deg, imp, bits];
     signal = soah5import(charinfo,cur_var,tech); % Import
     [switched, s_info] = sw_cycle(signal);
-    [yout, mse, ber, errors] = sw_filter(switched.y_s, switched.xs_slice, s_info);
-    errorDistPlot(s_info, errors, spTech);
-    saveas(gcf, [rootdir 'BERxGI_' spans{S} '_' sprintf('%imA_',int16(1e3*bias)),...
+    [~, ~, ber, errors] = sw_filter(switched.y_s, switched.xs_slice, s_info);
+    BERxGI{S,ib,T+1,ibits} = errorDistPlot(s_info, errors, [spans{S} ' - ' spTech]);
+    saveas(gcf, ['.\Results\BERxGI_' spans{S} '_' sprintf('%imA_',int16(1e3*bias)),...
          upper(spTech) '.png'], 'png');
     toc(tStart); close all;
 end
 end
 end
-
 end
-load handel
-sound(y(1:5e4),Fs)
+save('.\Results\BERxGI','BERxGI')
+% load handel
+% sound(y(1:5e4),Fs)
+system('shutdown -s')
